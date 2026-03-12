b48cc2cd707176cb25d9ce6d002ebc8cb2b92215
ZakaryaAanni <z.aanni@netopia.ma>
2026-03-12 14:06:32 +0000

    Add AuthDto for user authentication and update Kafka consumer: create AuthDto class, modify application.yaml for consumer configuration, and adjust ConsumerKafka to use AuthDto

--- course-service/src/main/java/com/example/courseservice/service/ConsumerKafka.java
+++ course-service/src/main/java/com/example/courseservice/service/ConsumerKafka.java
@@ -1,13 +1,13 @@
 package com.example.courseservice.service;
 
-import com.example.courseservice.dto.UserDto;
+import com.example.courseservice.dto.AuthDto;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerKafka {
     @KafkaListener(topics = "user-registered", groupId = "notification-group")
-    public void consume(UserDto event) {
+    public void consume(AuthDto event) {
 
         System.out.println("Sending notification to: " + event.getEmail());
 
